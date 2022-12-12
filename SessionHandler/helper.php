90e8ff08accf05811e5e403b4be9ff6a5c1b4fff
AliiAhmadi <aliahmadi.github@gmail.com>
2022-12-12 21:35:42 +0330

    Finish upload progress

--- SessionHandler/helper.php
+++ SessionHandler/helper.php
@@ -1,29 +1,79 @@
 <?php
 session_start();
 
+function CheckExtension($extension)
+{
+    $allowedExtensions = [
+        "jpg",
+        "jpag",
+        "json",
+        "pdf",
+        "txt",
+        "doc",
+        "rtf",
+        "gif",
+        "rar"
+    ];
+    if (in_array($extension, $allowedExtensions)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function sizeChecker($size)
+{
+    return ($size <= 50000);
+}
+
+
+function splitFile($data)
+{
+    return explode(".", $data);
+}
+
+
 $message = null;
 $status = false;
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if($_POST["submit"] == "Submit"){
-        if(isset($_POST["file"]) && !empty($_POST["file"])){
-            $GLOBALS["message"] = "file successfuly uploaded";
-            $GLOBALS["status"] = true;
-        }else{
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if ($_POST["submit"] == "Submit") {
+        if (isset($_FILES["file"]) && !empty($_FILES["file"])) {
+            $GLOBALS["fileName"] = $_FILES["file"]["name"];
+            $GLOBALS["fileSize"] = $_FILES["file"]["size"];
+            $GLOBALS["fileType"] = $_FILES["file"]["type"];
+
+            $fileOptions = splitFile($GLOBALS["fileName"]);
+            $fileOptions[1] = strtolower($fileOptions[1]);
+
+            if (CheckExtension($fileOptions[1]) && sizeChecker($GLOBALS["fileSize"])) {
+                $fileNameInServer = md5($GLOBALS["fileName"] . time()) . "." . $fileOptions[1];
+                $uploadLocation = "../uploads/";
+                $fullPath = $uploadLocation . $fileNameInServer;
+
+                if (move_uploaded_file($_FILES["file"]["tmp_name"], $fullPath)) {
+                    $GLOBALS["message"] = "file successfuly uploaded";
+                    $GLOBALS["status"] = true;
+                } else {
+                    $GLOBALS["message"] = "Error";
+                    $GLOBALS["status"] = false;
+                }
+            } else {
+                $GLOBALS["message"] = "This file is not allowed for upload";
+                $GLOBALS["status"] = false;
+            }
+        } else {
             $GLOBALS["message"] = "First select your file";
             $GLOBALS["status"] = false;
         }
-    }else{
+    } else {
         $GLOBALS["message"] = "Please select your file";
         $GLOBALS["status"] = true;
     }
-}else{
-        $GLOBALS["message"] = "Please select your file";
-        $GLOBALS["status"] = true;
+} else {
+    $GLOBALS["message"] = "Please select your file";
+    $GLOBALS["status"] = true;
 }
 $_SESSION["message"] = $message;
 $_SESSION["status"] = $status;
-
-var_dump($message);
-
+$_SESSION["size"] = $fileSize;
 header("location:index.php");
-?>
--- SessionHandler/index.php
+++ SessionHandler/index.php
@@ -1,35 +1,38 @@
 <?php
-    session_start();
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>File Uploader - Session Handler</title>
 </head>
+
 <body>
     <form action="<?php
-        echo "helper.php";
-    ?>" method="post" style="text-align: center;">
-    <label for="file" style="color: <?php
-        if(isset($_SESSION["status"]) && $_SESSION["status"]){
-            echo "green";
-        }else{
-            echo "red";
-        }
-    ?>;">
-        <?php 
-            if(isset($_SESSION["message"])){
+                    echo "helper.php";
+                    ?>" method="post" style="text-align: center;" enctype="multipart/form-data">
+        <label for="file" style="color: <?php
+                                        if (isset($_SESSION["status"]) && $_SESSION["status"]) {
+                                            echo "green";
+                                        } else {
+                                            echo "red";
+                                        }
+                                        ?>;">
+            <?php
+            if (isset($_SESSION["message"])) {
                 echo $_SESSION["message"];
             }
-        ?>
-    </label>
-    <br><br>
+            ?>
+        </label>
+        <br><br>
         <input type="file" name="file" id="file">
         <br><br>
         <input type="submit" value="Submit" name="submit">
     </form>
 </body>
+
 </html>
